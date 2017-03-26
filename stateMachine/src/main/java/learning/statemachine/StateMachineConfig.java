74e16f63b63b0930d1982d896125e57113e279e5
gaochunhua <gaochunhua@niwodai.net>
2017-03-26 14:05:00 +0800

    create spring annotation module:

---
+++ stateMachine/src/main/java/learning/statemachine/StateMachineConfig.java
@@ -0,0 +1,12 @@
+package learning.statemachine;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachine;
+
+/**
+ * Created by isaac on 17/03/2017.
+ */
+@Configuration
+@EnableStateMachine
+public class StateMachineConfig {
+}
